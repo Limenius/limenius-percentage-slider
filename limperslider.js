215629473c086d432691342f994f7323b2f7d03f
nacho <nacho@limenius.com>
2015-09-01 23:08:47 +0200

    Init hides inputs

--- limperslider.js
+++ limperslider.js
@@ -1,43 +1,52 @@
 'use strict';
 
 var limperPrototype = {
+    element: null,
 
-  init: function (selectors, options) {
-    var elements = [];
-    var idlimper = '';
-    var lastelement = null;
-    for (var i = 0; i < selectors.length; i++) {
-        var selector = selectors[i];
-        var element = document.querySelector(selector);
-        if (!selector) {
-            throw "element" + selector + "does not exist"
+    hideInputs: function(selectors) {
+        for (var i = 0; i < selectors.length; i++) {
+            var selector = selectors[i];
+            var element = document.querySelector(selector);
+            element.setAttribute('type', 'hidden');
         }
-        var id = element.getAttribute('id');
-        if (i > 0) {
-            idlimper += "-";
+    },
+
+    init: function (selectors, options) {
+        var elements = [];
+        var idlimper = '';
+        var lastelement = null;
+        for (var i = 0; i < selectors.length; i++) {
+            var selector = selectors[i];
+            var element = document.querySelector(selector);
+            if (!selector) {
+                throw "element" + selector + "does not exist"
+            }
+            var id = element.getAttribute('id');
+            if (i > 0) {
+                idlimper += "-";
+            }
+            idlimper += id;
+            elements.push(element);
+            lastelement = element;
         }
-        idlimper += id;
-        elements.push(element);
-        lastelement = element;
-    }
 
-    idlimper += "-limper";
-    var lelement = document.createElement('div');
-    lastelement.insertAdjacentElement('afterend', lelement);
-    lelement.setAttribute('id', idlimper);
-  },
+        idlimper += "-limper";
+        this.element = document.createElement('div');
+        lastelement.insertAdjacentElement('afterend', this.element);
+        this.element.setAttribute('id', idlimper);
 
+        this.hideInputs(selectors);
+    },
 };
- 
- 
+
 function LimperSlider(selectors, options) {
 
-  function F() {};
-  F.prototype = limperPrototype;
+    function F() {};
+    F.prototype = limperPrototype;
 
-  var f = new F();
+    var f = new F();
 
-  f.init(selectors, options);
-  return f;
+    f.init(selectors, options);
+    return f;
  
 }
--- spec/limperslider.js
+++ spec/limperslider.js
@@ -1,12 +1,27 @@
 'use strict';
 
 describe("LimperSlider", function() {
-  var limperslider;
+    var limperslider;
 
-  it("Initialization creates div", function() {
-    limperslider = new LimperSlider(["#percentage1", "#percentage2", "#percentage3"]);
-    var element = document.querySelector('#percentage1-percentage2-percentage3-limper');
-    expect(element).toBeTruthy();
-  });
+    it("Initialization creates div", function() {
+        limperslider = new LimperSlider(["#percentage1", "#percentage2", "#percentage3"]);
+        var element = document.querySelector('#percentage1-percentage2-percentage3-limper');
+        expect(element).toBeTruthy();
+    })  ;
 
+    it("Bad initialization throws exception", function() {
+        var thrower = function() {
+            new LimperSlider(["#yolo", "#percentage2", "#percentage3"]);
+        }
+        expect(thrower).toThrow();
+    });
+
+    it("Hides inputs", function() {
+        var thrower = function() {
+            new LimperSlider(["#percentage1", "#percentage2", "#percentage3"]);
+        }
+        var element = document.querySelector("#percentage2");
+        var type = element.getAttribute('type');
+        expect(type).toEqual('hidden');
+    });
 });
